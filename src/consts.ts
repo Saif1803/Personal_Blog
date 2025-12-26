19f85f4b02e6f6491b1352b56f3ea58592e37b04
saif <saif48763@gmail.com>
2025-12-26 18:59:10 +0530

    title fix 2

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Blogs | Posts on Tech and Book Reviews';
+export const SITE_TITLE = 'Blog | Posts on Tech and Book Reviews';
 export const SITE_DESCRIPTION = 'My Personal Blog where I share my thoughts on Tech and Book Reviews';
