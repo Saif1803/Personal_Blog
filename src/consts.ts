d8b56b24278f70637890044303017237cbe49824
saif <saif48763@gmail.com>
2026-01-11 23:08:25 +0530

    description fix

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Blog | Posts on Tech and Book Reviews';
-export const SITE_DESCRIPTION = 'My Personal Blog where I share my thoughts on Tech and Book Reviews';
+export const SITE_DESCRIPTION = 'My Personal Blog where I share my thoughts and views on whatever interests me';
